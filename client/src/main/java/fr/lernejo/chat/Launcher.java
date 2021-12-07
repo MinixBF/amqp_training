0987317ec3a25bb5f8ce87e714c157adb85ea5e3
MinixBF <benj.ferre@hotmail.fr>
2021-12-07 16:03:37 +0100

    Client AMQP

---
+++ client/src/main/java/fr/lernejo/chat/Launcher.java
@@ -0,0 +1,30 @@
+package fr.lernejo.chat;
+
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class Launcher {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        boolean start = true;
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+
+        while(start) {
+            System.out.println("Input a message, we will sent it for you (q for quit)");
+            input = scanner.nextLine();
+            if(input.equals("q")) {
+                start = false;
+            }else {
+                rabbitTemplate.convertAndSend("chat_messages", input);
+            }
+        }
+    }
+}
+
--- server/src/main/java/fr/lernejo/chat/ChatMessageRepository.java
+++ server/src/main/java/fr/lernejo/chat/ChatMessageRepository.java
@@ -1,6 +1,5 @@
 package fr.lernejo.chat;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ChatMessageRepository {
